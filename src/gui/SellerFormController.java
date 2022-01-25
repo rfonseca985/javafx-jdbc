667b5bd9e4c39b66c7388d7ee75ed13e401ea926
rfonseca985 <rfonseca985@gmail.com>
2022-01-25 16:06:20 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -83,6 +85,83 @@ public class SellerFormController implements Initializable {
 
 	private ObservableList<Department> obsList;
 
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		} catch (ValidationExcepition e) {
+			setErrorMessages(e.getErrors());
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListener) {
+			listener.onDataChanged();
+		}
+
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+
+		ValidationExcepition exception = new ValidationExcepition("Validation error");
+
+		obj.setId(Utils.TryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
+		obj.setName(txtName.getText());
+		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		}
+		else {
+		Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+		obj.setBirthDate(Date.from(instant));
+		}
+		
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.TryParseToDouble(txtBaseSalary.getText()));;
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initialzeNodes();
+
+	}
+	
 	public void setSeller(Seller entity) {
 		this.entity = entity;
 	}
@@ -127,9 +206,14 @@ public class SellerFormController implements Initializable {
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		labelErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+		
+		labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+		
+		labelErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
+
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
+			
 	}
 
 	private void initializeComboBoxDepartment() {
@@ -144,62 +228,6 @@ public class SellerFormController implements Initializable {
 		comboBoxDepartment.setButtonCell(factory.call(null));
 	}
 
-	@FXML
-	public void onBtSaveAction(ActionEvent event) {
-		if (entity == null) {
-			throw new IllegalStateException("Entity was null");
-		}
-		if (service == null) {
-			throw new IllegalStateException("Service was null");
-		}
-		try {
-			entity = getFormData();
-			service.saveOrUpdate(entity);
-			notifyDataChangeListeners();
-			Utils.currentStage(event).close();
-		} catch (ValidationExcepition e) {
-			setErrorMessages(e.getErrors());
-		} catch (DbException e) {
-			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
-		}
-	}
-
-	private void notifyDataChangeListeners() {
-		for (DataChangeListener listener : dataChangeListener) {
-			listener.onDataChanged();
-		}
-
-	}
-
-	private Seller getFormData() {
-		Seller obj = new Seller();
-
-		ValidationExcepition exception = new ValidationExcepition("Validation error");
-
-		obj.setId(Utils.TryParseToInt(txtId.getText()));
-
-		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
-			exception.addError("name", "Field can't be empty");
-		}
-		obj.setName(txtName.getText());
-
-		if (exception.getErrors().size() > 0) {
-			throw exception;
-		}
-
-		return obj;
-	}
-
-	@FXML
-	public void onBtCancelAction(ActionEvent event) {
-		Utils.currentStage(event).close();
-	}
-
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		initialzeNodes();
-
-	}
 
 	private void initialzeNodes() {
 		Constraints.setTextFieldInteger(txtId);
